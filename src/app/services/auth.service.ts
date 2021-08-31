f61e9f9cd567869e0f10ddb4ca73165dad7badac
PasztorGergo <pasztorg05@gmail.com>
2021-08-31 14:59:50 +0200

    authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-import {Auth} from 'firebase/auth'
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 
